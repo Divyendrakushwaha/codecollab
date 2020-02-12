d1834b939b95163eac22572cda90eb4c46c37689
Raghavsangal <raghavsinghal11@gmail.com>
2020-02-13 01:06:41 +0530

    karan

---
+++ passport.js
@@ -0,0 +1,82 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var mongoUtil = require( './lib/mongoUtil' );
+var User=require('./models/user');
+mongoUtil.connectToServer( function( err, client ) {
+    if (err) console.log(err);
+    // start the rest of your app here
+    // var mongoUtil = require( 'mongoUtil' );
+  var db = mongoUtil.getDb();
+  var cl1=db.collection( 'User' );
+passport.serializeUser(function (user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function (id, done) {
+  cl1.findOne({_id: id}, function (err, user) {
+    done(err, user);
+  })
+});
+
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+  },
+  function (username, password, done) {
+    console.log(cl1.findOne({email: username}));
+    cl1.findOne({email: username}, function (err, user) {
+      if (err) return done(err);
+      if (!user) {
+        return done(null, false, {
+          message: 'Incorrect username or password'
+        });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, {
+          message: 'Incorrect username or password'
+        });
+      }
+
+      return done(null, user);
+    })
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: '612822675590167',
+    clientSecret: 'dcb63fc3264a53bc54496cad1ab51da6',
+    callbackURL: 'https://salty-bayou-80249.herokuapp.com/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'email']
+  },
+  function(token, refreshToken, profile, done) {
+    User.findOne({'facebookId': profile.id}, function(err, user) {
+      if (err) return done(err);
+
+      if (user) {
+        return done(null, user);
+      } else {
+        User.findOne({email: profile.emails[0].value}, function (err, user) {
+          if (user) {
+            user.facebookId = profile.id
+            return user.save(function (err) {
+              if (err) return done(null, false, { message: "Can't save user info"});
+              return done(null, user);
+            })
+          }
+
+          var user = new User();
+          user.name = profile.displayName;
+          user.email = profile.emails[0].value;
+          user.facebookId = profile.idea
+          user.save(function (err) {
+            if (err) return done(null, false, { message: "Can't save user info"});
+            return done(null, user);
+          });
+        })
+      }
+
+
+    });
+  }
+));
+});
---
+++ routes/auth.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var mongoUtil = require( './../lib/mongoUtil' );
+var User=require('./../models/user');
+mongoUtil.connectToServer( function( err, client ) {
+  if (err) console.log(err);
+  // start the rest of your app here
+  // var mongoUtil = require( 'mongoUtil' );
+var db = mongoUtil.getDb();
+var cl1=db.collection( 'User' );
+router.route('/login')
+  .get(function(req, res, next) {
+    res.render('login', { title: 'Login your account'});
+  })
+  .post(passport.authenticate('local', {
+    failureRedirect: '/login'
+  }), function (req, res) {
+    res.redirect('/');
+  });
+
+router.route('/register')
+  .get(function(req, res, next) {
+    res.render('register', { title: 'Register a new account'});
+  })
+  .post(function(req, res, next) {
+    req.checkBody('name', 'Empty Name').notEmpty();
+    req.checkBody('email', 'Invalid Email').isEmail();
+    req.checkBody('password', 'Empty Password').notEmpty();
+    req.checkBody('password', 'Password do not match').equals(req.body.confirmPassword).notEmpty();
+    console.log(req.body);
+    var errors = req.validationErrors();
+    if (errors) {
+      res.render('register', {
+        name: req.body.name,
+        email: req.body.email,
+        errorMessages: errors
+      });
+    } else {
+      var user = new User();
+      user.name = req.body.name;
+      user.email = req.body.email;
+      user.setPassword(req.body.password);
+    //   console.log(db.user)
+    cl1.insertOne(user, function(err, res) {
+        if (err) {
+                  res.render('register', {errorMessages: err});
+                } else {
+                  res.redirect('/login');
+                }
+            });
+            // db.close();
+    //   user.save(function (err) {
+    //     if (err) {
+    //       res.render('register', {errorMessages: err});
+    //     } else {
+    //       res.redirect('/login');
+    //     }
+    //   })
+    }
+  });
+
+router.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
+
+router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/',
+  failureRedirect: '/'
+}));
+
+// console.log(db)
+
+} );
+// var d
+
+
+
+module.exports = router;
